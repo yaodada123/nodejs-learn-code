5c462a4e405ec6fd360a38264d06c262609f4f54
yhc <2580577036@qq.com>
2022-04-10 20:53:23 +0800

    path-l-f

--- fs-learn/wright-l.js
+++ fs-learn/wright-l.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
+const path = require("path");
 
-fs.writeFile("./files/w.txt","121231",'utf-8', (err) => {
+const pathStr = path.join(__dirname, "./files/w.txt");
+
+console.log( path.basename(pathStr, '.txt'));
+console.log( path.extname(pathStr) );
+
+fs.writeFile( pathStr,"121231",'utf-8', (err) => {
     if(err) {
         return console.log("文件写入错误！ " + err.message);
     }
-    return console.log("文件写入成功！");
+    return console.log("文件写入成功！``");
 })
 
